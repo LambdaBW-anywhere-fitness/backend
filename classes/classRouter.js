4815c8abc8e255675e91efaae11543ec45eee076
toddmurphy <toddmurphy93@gmail.com>
2020-02-02 21:41:42 -0400

    getClassById endpoint working and returing a single class by id

--- classes/classRouter.js
+++ classes/classRouter.js
@@ -17,5 +17,22 @@ router.get('/', (req, res) => {
 });
 
 //getClassById --> gets a list a single 'class' by 'id' --> from endpoint --> /api/classes/:id
+router.get('/:id', (req, res) => {
+  const classId = req.params.id;
+
+  Classes.getClassById(classId)
+    .then(activity => {
+      console.log('inside getClassById', activity);
+      if (activity) {
+        res.status(200).json(activity);
+      } else {
+        res.status(401).json({ message: 'Sorry, class with that id not found' });
+      }
+    })
+    .catch(error => {
+      console.log('inside getClassById error', error);
+      res.status(500).json({ message: 'Sorry, class with that id not returned from the server', error });
+    });
+});
 
 module.exports = router;
