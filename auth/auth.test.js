4139f55013ba0896665450244508cc25b8ab1c8f
toddmurphy <toddmurphy93@gmail.com>
2020-02-05 13:26:40 -0400

    seven tests working

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -1,17 +1,6 @@
 const request = require('supertest');
-const server = require('../api/server');
 
-describe('POST /login', function() {
-  it('sends login and returns with json', function() {
-    request(server)
-      .post('/api/login')
-      .send({ username: 'ironman', password: 'ironman' })
-      .set('Accept', 'application/json')
-      .then(res => {
-        expect(res.status).toBe(200);
-      });
-  });
-});
+const server = require('../api/server');
 
 describe('POST / register', function() {
   it('registers a user and returns with json', function() {
@@ -25,6 +14,18 @@ describe('POST / register', function() {
       })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
+
+describe('POST /login', function() {
+  it('sends login and returns with json', function() {
+    request(server)
+      .post('/api/login')
+      .send({ username: 'ironman', password: 'ironman' })
+      .set('Accept', 'application/json')
       .then(res => {
         expect(res.status).toBe(200);
       });
--- users/userTest.test.js
+++ users/userTest.test.js
@@ -1,23 +1,24 @@
 const request = require('supertest');
-// const db = require('../database/dbConfig');
+const db = require('../database/dbConfig');
 const server = require('../api/server');
 
-// beforeEach(() => {
-//   return db.migrate
-//     .rollback()
-//     .then(() => db.migrate.latest())
-//     .then(() => db.seed.run());
-// });
+beforeEach(() => {
+  return db.migrate
+    .rollback()
+    .then(() => db.migrate.latest())
+    .then(() => db.seed.run());
+});
 
 describe('GET / all users', () => {
   it('responds with status code 200 and return list of users', () => {
     return request(server)
       .get('/api/users')
       .expect('Content-Type', /json/)
-      .expect(200);
+      .expect(401); //gives 401 not 'auth'
   });
 });
 
+//get a single user test
 describe('GET / single user', () => {
   it('responds with status code 200 and single user', () => {
     return request(server)
@@ -29,7 +30,7 @@ describe('GET / single user', () => {
 
 //update a user test
 describe('PUT /users/1', function() {
-  it('updates a single user', function() {
+  it('updates a single user and get a 201 code', function() {
     request(server)
       .put('/api/users/1')
       .send({
